0c0b38c241e1494c7c5d8fb6b275700577dbe150
Fausto V <soleogrim@gmail.com>
2026-02-01 23:57:21 -0500

    Module 5: Learned Variable naming, when to and how to effectively use whitespace for code readability, comments, and  converting a string message into a char array while learning reversing an array

---
+++ Module-5/Program.cs
@@ -0,0 +1,116 @@
+﻿// ** unit 2 **
+// variable names start with alphanumeric characters and _ 
+// no special characters like #, - , $ are allowed
+// Using an underscore character to start a variable name is typically reserved for private instance fields
+// variable names must not be C# keywords like float, float; , string string;
+// variable names are case sensitive so string MyValye; and string myValue are two different vaiables
+// camelcase, variable makes sense like idOrder not idOrd, dont incluce the datatype in variables and descritive and meaningful
+
+//  ** Unit 3 **
+
+/*
+string firstName = "Bob";
+int widgetsPurchased = 7;
+// Testing a change to the message.
+// int widgetsSold = 7;
+// Console.WriteLine($"{firstName} sold {widgetsSold} widgets.");
+Console.WriteLine($"{firstName} purchased {widgetsPurchased} widgets.");
+*/
+
+/*
+  The following code creates five random OrderIDs
+  to test the fraud detection process.  OrderIDs 
+  consist of a letter from A to E, and a three
+  digit number. Ex. A123.
+*/
+// I like this
+
+/*
+Random random = new Random();
+string[] orderIDs = new string[5];
+
+for (int i = 0; i < orderIDs.Length; i++)
+{
+    int prefixValue = random.Next(65, 70);
+    string prefix = Convert.ToChar(prefixValue).ToString();
+    string suffix = random.Next(1, 1000).ToString("000");
+
+    orderIDs[i] = prefix + suffix;
+}
+
+foreach (var orderID in orderIDs)
+{
+    Console.WriteLine(orderID);
+}
+*/
+
+
+// ** Unit 4 **
+
+
+/*
+// Example 1:
+Console
+.
+WriteLine
+(
+"Hello Example 1!"
+)
+;
+
+// Example 2:
+string firstWord="Hello";string lastWord="Example 2";Console.WriteLine(firstWord+" "+lastWord+"!");
+*/
+
+/*
+Random dice = new Random();
+
+int roll1 = dice.Next(1, 7);
+int roll2 = dice.Next(1, 7);
+int roll3 = dice.Next(1, 7);
+
+int total = roll1 + roll2 + roll3;
+Console.WriteLine($"Dice roll: {roll1} + {roll2} + {roll3} = {total}");
+
+if ((roll1 == roll2) || (roll2 == roll3) || (roll1 == roll3)) 
+{
+    if ((roll1 == roll2) && (roll2 == roll3)) 
+    {
+        Console.WriteLine("You rolled triples!  +6 bonus to total!");
+        total += 6; 
+    } 
+    else 
+    {
+        Console.WriteLine("You rolled doubles!  +2 bonus to total!");
+        total += 2;
+    }
+}
+*/
+
+//  ** Unit 5 **
+
+/* In this code we will be converting our string variable to a character array
+and reversing the message then we will count how many o's are in our string variable
+and print out the new message wile also showing how many o's appeared in the message
+*/
+
+
+string originalMessage = "The quick brown fox jumps over the lazy dog.";
+
+char[] message = originalMessage.ToCharArray();
+Array.Reverse(message);
+
+int letterCount = 0;
+
+foreach (char letter in message) 
+{ 
+    if (letter == 'o') 
+    { 
+        letterCount++; 
+    } 
+}
+
+string newMessage = new String(message);
+
+Console.WriteLine(newMessage);
+Console.WriteLine($"'o' appears {letterCount} times.");
