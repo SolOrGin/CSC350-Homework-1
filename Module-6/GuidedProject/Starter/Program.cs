6b2ca3af66fac40343cd414f038167c2d0693859
Fausto V <soleogrim@gmail.com>
2026-02-02 03:02:00 -0500

    Module 6: created new array of studentScores, a string variable currentStudent inside the foreach loop and assigned the name to it to understand the logic behind our code, created if and else if statements to assign the scores from the current student that is in the looo and assigning it to the new array then created a new int variable, two actually one for the sum of assigned scores and the other for the current student grade then moved away from sophia to focus on any student that is being looped in order to get their average while displaying their name and grade before looping to the next student in the array

--- Module-6/GuidedProject/Starter/Program.cs
+++ Module-6/GuidedProject/Starter/Program.cs
@@ -4,30 +4,53 @@
 int currentAssignments = 5;
 
 int[] sophiaScores = new int[] {90, 86, 87, 98, 100};
-int[] andrewScore = new int[] {92, 89, 81, 96, 90};
-int[] emmaScore = new int[] {90, 85, 87, 98, 68};
-int[] loganScore = new int[] {90, 95, 87, 88, 96};
+int[] andrewScores = new int[] {92, 89, 81, 96, 90};
+int[] emmaScores = new int[] {90, 85, 87, 98, 68};
+int[] loganScores = new int[] {90, 95, 87, 88, 96};
 
 string[] studentNames = new string[] { "Sophia", "Andrew", "Emma", "Logan" };
 
+int[] studentScores = new int[10];
+
 foreach (string name in studentNames)
 {
+    string currentStudent = name;    // so by creating a new variable currentStudent it makes it easier to follow the logic in the code
+    // name will loop through the array and currentStudent will make it easy to think about the current student in the looped array
+    // as well as checking if the current student follows the if statement(s) (else if)
+    
     //Console.WriteLine($"{name}");     //so I noticed that in c++ you do endl; for a new line but in c# you dont need to do that it does it automatically
-    if (name == "Sophia")
+    
+    if (currentStudent == "Sophia")
+        studentScores = sophiaScores;  // so we dont need the curly brackets?  okay we do need them but just like c++ we dont need them if one statement is inside the anf if statement
+    
+    else if (currentStudent == "Andrew")
+        studentScores = andrewScores;
+    
+    else if (currentStudent == "Emma")
+        studentScores = emmaScores;
+
+    else if (currentStudent == "Logan")
+        studentScores = loganScores;
+
+    
+    int sumAssignmentScores = 0;
+
+    decimal currentStudentGrade = 0;
+
+    foreach (int score in studentScores)
     {
-        int sophiaSum = 0;
-        decimal sophiaScore;
-
-        foreach (int score in sophiaScores)
-        {
-            sophiaSum += score;
-        }
-
-        sophiaScore = (decimal)sophiaSum / currentAssignments;  //so here we convert the in sophiaSum to a decimal or in c++ a float
-
-        Console.WriteLine("Student\t\tGrade\n");                    //I see we can use \t for tab and \n for new line like in c++
-        Console.WriteLine("Sophia:\t\t" + sophiaScore + "\tA-");
+        sumAssignmentScores += score;
     }
+
+    currentStudentGrade = (decimal)sumAssignmentScores/ currentAssignments;  
+    //so here we convert the in sophiaSum to a decimal or in c++ a float
+    //moving away from sophia now and making it anyone, any student. 
+    // why is it (decimal)(sumAssignmentScores)? okay I see this is optional and safe but looks like optional
+
+    //Console.WriteLine("Student\t\tGrade\n");                    //I see we can use \t for tab and \n for new line like in c++
+    //Console.WriteLine("Sophia:\t\t" + sophiaScore + "\tA-");
+
+    Console.WriteLine($"{currentStudent}\t\t{currentStudentGrade}\t?");
 }
 
 
